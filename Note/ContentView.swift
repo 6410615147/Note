60668005f8289a9c7f418eb72c15b368e8e7c7ee
6410615147 <6410615147@student.tu.ac.th>
2024-04-02 20:21:29 +0700

    Initial Commit

---
+++ Note/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Note
+//
+//  Created by Babypowder on 2/4/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Note/NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteApp.swift
+//  Note
+//
+//  Created by Babypowder on 2/4/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
